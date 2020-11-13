9282d3cbbdb7b9964e30e669acf39a745cb4c3a1
Pavels Sisojevs <p.sisoyev@gmail.com>
2020-11-13 21:58:41 +0100

    Handle service errors in routes

--- route/src/main/scala/com/psisoyev/train/station/StationRoutes.scala
+++ route/src/main/scala/com/psisoyev/train/station/StationRoutes.scala
@@ -1,31 +1,42 @@
 package com.psisoyev.train.station
 
-import cats.{ Defer, MonadError }
+import cats.{ Defer, Monad }
 import cats.implicits._
-import com.psisoyev.train.station.arrival.Arrivals
-import com.psisoyev.train.station.arrival.Arrivals.{ Arrival, ArrivalError }
+import com.psisoyev.train.station.arrival.{ ArrivalValidator, Arrivals }
+import com.psisoyev.train.station.arrival.Arrivals.Arrival
+import com.psisoyev.train.station.arrival.ArrivalValidator.ArrivalError
 import com.psisoyev.train.station.departure.Departures
 import com.psisoyev.train.station.departure.Departures.{ Departure, DepartureError }
 import org.http4s.dsl.Http4sDsl
 import org.http4s.{ HttpRoutes, _ }
 import org.http4s.circe._
 import org.http4s.circe.CirceEntityEncoder._
+import tofu.Handle
+import tofu.syntax.handle._
 
-class StationRoutes[F[_]: MonadError[*[_], Throwable]: Defer: JsonDecoder](
+class StationRoutes[F[_]: Monad: Defer: JsonDecoder](
   arrivals: Arrivals[F],
+  arrivalValidator: ArrivalValidator[F],
   departures: Departures[F]
+)(
+  implicit
+  E1: Handle[F, ArrivalError],
+  E2: Handle[F, DepartureError]
 ) extends Http4sDsl[F] {
   val routes: HttpRoutes[F] = HttpRoutes.of[F] {
     case req @ POST -> Root / "arrival" =>
-      req
+      val res = req
         .asJsonDecode[Arrival]
-        .flatMap(arrivals.register)
-        .flatMap(_.fold(handleArrivalErrors, _ => Ok()))
+        .flatMap(arrivalValidator.validate)
+        .flatMap(arrivals.register) *> Ok()
+
+      E1.handleWith(res)(handleArrivalErrors)
     case req @ POST -> Root / "departure" =>
-      req
+      val res = req
         .asJsonDecode[Departure]
         .flatMap(departures.register) *> Ok()
 
+      E2.handleWith(res)(handleDepartureErrors)
   }
 
   def handleArrivalErrors: ArrivalError => F[Response[F]] = {
