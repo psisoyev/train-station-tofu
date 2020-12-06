76f5b95740afeb0bb61040622b6f07e5f1d93532
Pavels Sisojevs <p.sisoyev@gmail.com>
2020-12-06 12:19:24 +0100

    Extract generic tracing method

--- route/src/main/scala/com/psisoyev/train/station/StationRoutes.scala
+++ route/src/main/scala/com/psisoyev/train/station/StationRoutes.scala
@@ -1,23 +1,28 @@
 package com.psisoyev.train.station
 
-import cats.{ Defer, FlatMap, Monad }
 import cats.implicits._
-import com.psisoyev.train.station.arrival.{ ArrivalValidator, Arrivals }
-import com.psisoyev.train.station.arrival.Arrivals.Arrival
+import cats.{ Defer, FlatMap, Monad }
+import com.psisoyev.train.station.Tracing.withNewTrace
 import com.psisoyev.train.station.arrival.ArrivalValidator.ArrivalError
+import com.psisoyev.train.station.arrival.Arrivals.Arrival
+import com.psisoyev.train.station.arrival.{ ArrivalValidator, Arrivals }
 import com.psisoyev.train.station.departure.Departures
 import com.psisoyev.train.station.departure.Departures.{ Departure, DepartureError }
+import cr.pulsar.Producer
+import org.http4s.circe.CirceEntityEncoder._
+import org.http4s.circe._
 import org.http4s.dsl.Http4sDsl
 import org.http4s.{ HttpRoutes, _ }
-import org.http4s.circe._
-import org.http4s.circe.CirceEntityEncoder._
 import tofu.generate.GenUUID
-import tofu.{ Handle, WithRun }
-import tofu.syntax.context._
+import tofu.{ Handle, HasProvide }
 
-class StationRoutes[I[_]: Monad: Defer: JsonDecoder: GenUUID, F[_]: FlatMap: WithRun[*[_], I, Ctx]](
+class StationRoutes[
+  I[_]: Monad: Defer: JsonDecoder: GenUUID,
+  F[_]: FlatMap: HasProvide[*[_], I, Ctx]
+](
   arrivals: Arrivals[F],
   arrivalValidator: ArrivalValidator[F],
+  producer: Producer[I, Event],
   departures: Departures[F]
 )(implicit
   E1: Handle[I, ArrivalError],
@@ -28,22 +33,18 @@ class StationRoutes[I[_]: Monad: Defer: JsonDecoder: GenUUID, F[_]: FlatMap: Wit
       val res = req
         .asJsonDecode[Arrival]
         .flatMap { arrival =>
-          traced(arrivalValidator.validate(arrival).flatMap(arrivals.register)) *> Ok()
-        }
+          val registration = arrivalValidator.validate(arrival).flatMap(arrivals.register)
+          withNewTrace(registration).flatMap(producer.send_)
+        } *> Ok()
 
       E1.handleWith(res)(handleArrivalErrors)
     case req @ POST -> Root / "departure" =>
       val res = req
         .asJsonDecode[Departure]
-        .flatMap(departure => traced(departures.register(departure))) *> Ok()
+        .flatMap(departure => withNewTrace(departures.register(departure))) *> Ok()
 
       E2.handleWith(res)(handleDepartureErrors)
   }
-
-  def traced[T](action: F[T]): I[T] =
-    I.randomUUID.map(id => TraceId(id.toString)).flatMap { traceId =>
-      runContext(action)(Ctx(traceId))
-    }
 
   def handleArrivalErrors: ArrivalError => I[Response[I]] = { case ArrivalError.UnexpectedTrain(id) =>
     BadRequest(s"Unexpected train $id")
