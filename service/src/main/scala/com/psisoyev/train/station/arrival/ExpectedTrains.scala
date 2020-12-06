56b38115c2f0299886f914fd7d644e8fe745b2e4
Pavels Sisojevs <p.sisoyev@gmail.com>
2020-12-06 12:18:34 +0100

    Replace cats.Ref with zio.Ref

--- service/src/main/scala/com/psisoyev/train/station/arrival/ExpectedTrains.scala
+++ service/src/main/scala/com/psisoyev/train/station/arrival/ExpectedTrains.scala
@@ -1,10 +1,9 @@
 package com.psisoyev.train.station.arrival
 
-import cats.Functor
-import cats.implicits._
-import cats.effect.concurrent.Ref
-import com.psisoyev.train.station.{ Expected, From, TrainId }
 import com.psisoyev.train.station.arrival.ExpectedTrains.ExpectedTrain
+import com.psisoyev.train.station.{ Expected, From, TrainId }
+import tofu.lift.Lift
+import zio.{ Ref, UIO }
 
 trait ExpectedTrains[F[_]] {
   def get(id: TrainId): F[Option[ExpectedTrain]]
@@ -15,11 +14,11 @@ trait ExpectedTrains[F[_]] {
 object ExpectedTrains {
   case class ExpectedTrain(from: From, time: Expected)
 
-  def make[F[_]: Functor](
-    ref: Ref[F, Map[TrainId, ExpectedTrain]]
+  def make[F[_]: Lift[UIO, *[_]]](
+    ref: Ref[Map[TrainId, ExpectedTrain]]
   ): ExpectedTrains[F] = new ExpectedTrains[F] {
-    override def get(id: TrainId): F[Option[ExpectedTrain]]         = ref.get.map(_.get(id))
-    override def remove(id: TrainId): F[Unit]                       = ref.update(_.removed(id))
-    override def update(id: TrainId, train: ExpectedTrain): F[Unit] = ref.update(_.updated(id, train))
+    override def get(id: TrainId): F[Option[ExpectedTrain]]         = F.lift(ref.get.map(_.get(id)))
+    override def remove(id: TrainId): F[Unit]                       = F.lift(ref.update(_.removed(id)))
+    override def update(id: TrainId, train: ExpectedTrain): F[Unit] = F.lift(ref.update(_.updated(id, train)))
   }
 }
