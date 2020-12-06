eee79babfb6aa37860ca112fc923d345b7431040
Pavels Sisojevs <p.sisoyev@gmail.com>
2020-12-06 12:19:45 +0100

    Simplify effect constraints, add producer/consumer loggers

--- server/src/main/scala/com/psisoyev/train/station/Resources.scala
+++ server/src/main/scala/com/psisoyev/train/station/Resources.scala
@@ -1,29 +1,25 @@
 package com.psisoyev.train.station
 
-import cats.effect.concurrent.Ref
-import cats.effect.{ Concurrent, ContextShift, Resource }
+import cats.effect.{ Concurrent, ContextShift, Resource, Sync }
 import cats.implicits._
 import cats.{ Inject, Parallel }
-import com.psisoyev.train.station.arrival.ExpectedTrains.ExpectedTrain
 import cr.pulsar.{ Consumer, Producer, Pulsar, Subscription, Topic, Config => PulsarConfig }
 import io.circe.Encoder
-import tofu.lift.Lift
 import tofu.logging.{ Logging, Logs }
-import tofu.syntax.lift._
 
-final case class Resources[F[_], E](
+final case class Resources[I[_], F[_], E](
   config: Config,
-  producer: Producer[F, E],
-  consumers: List[Consumer[F, E]],
+  producer: Producer[I, E],
+  consumers: List[Consumer[I, E]],
   logger: Logging[F]
 )
 
 object Resources {
   def make[
-    I[_]: Concurrent: ContextShift,
-    F[_]: Concurrent: ContextShift: Parallel,
+    I[_]: Concurrent: ContextShift: Parallel,
+    F[_]: Sync,
     E: Inject[*, Array[Byte]]: Encoder
-  ]: Resource[I, Resources[F, E]] = {
+  ]: Resource[I, Resources[I, F, E]] = {
     def topic(config: PulsarConfig, city: City) =
       Topic
         .Builder
@@ -32,7 +28,7 @@ object Resources {
         .withType(Topic.Type.Persistent)
         .build
 
-    def consumer(client: Pulsar.T, config: Config, city: City): Resource[I, Consumer[F, E]] = {
+    def consumer(client: Pulsar.T, config: Config, city: City)(implicit L: Logging[I]): Resource[I, Consumer[I, E]] = {
       val name         = s"${city.value}-${config.city.value}"
       val subscription =
         Subscription
@@ -41,20 +37,20 @@ object Resources {
           .withType(Subscription.Type.Failover)
           .build
 
-      Consumer.create[I, F, E](client, topic(config.pulsar, city), subscription)
+      Consumer.withLogger[I, E](client, topic(config.pulsar, city), subscription, EventLogger.incomingEvents)
     }
 
-    def producer(client: Pulsar.T, config: Config): Resource[I, Producer[F, E]] =
-      Producer.create[I, F, E](client, topic(config.pulsar, config.city))
-
-    val logs: Logs[I, F] = Logs.sync[I, F]
+    def producer(client: Pulsar.T, config: Config)(implicit L: Logging[I]): Resource[I, Producer[I, E]] =
+      Producer.withLogger[I, E](client, topic(config.pulsar, config.city), EventLogger.outgoingEvents)
 
     for {
-      config    <- Resource.liftF(Config.load[I])
-      client    <- Pulsar.create[I](config.pulsar.url)
-      global    <- Resource.liftF(logs.byName("global"))
-      producer  <- producer(client, config)
-      consumers <- config.connectedTo.traverse(consumer(client, config, _))
-    } yield Resources(config, producer, consumers, global)
+      config         <- Resource.liftF(Config.load[I])
+      client         <- Pulsar.create[I](config.pulsar.url)
+      consumerLogger <- Resource.liftF(Logs.sync[I, I].byName("consumer"))
+      producerLogger <- Resource.liftF(Logs.sync[I, I].byName("producer"))
+      global         <- Resource.liftF(Logs.sync[I, F].byName("global"))
+      producer       <- producer(client, config)(producerLogger)
+      consumers      <- config.connectedTo.traverse(consumer(client, config, _)(consumerLogger))
+    } yield Resources[I, F, E](config, producer, consumers, global)
   }
 }
