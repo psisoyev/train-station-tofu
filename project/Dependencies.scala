3dac619fb93999d0f5e2b08160dbf5fb97ed989c
Pavels Sisojevs <p.sisoyev@gmail.com>
2021-01-17 01:41:55 +0000

    Update kind-projector to 0.11.3

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -56,7 +56,7 @@ object Version {
   val zioCats = "2.2.0.1"
   val zio = "1.0.3"
   val fs2Core = "2.4.2"
-  val kindProjector = "0.11.2"
+  val kindProjector = "0.11.3"
   val ciris = "1.2.1"
   val http4s = "1.0-107-6676c1e"
   val circe = "0.13.0"
