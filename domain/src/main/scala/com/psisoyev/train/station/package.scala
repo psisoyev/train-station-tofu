4603aab7c0c9974fed55401db9d8d79ec2d47b02
Pavels Sisojevs <p.sisoyev@gmail.com>
2021-02-27 16:27:03 +0100

    Use derevo to derive instances

--- domain/src/main/scala/com/psisoyev/train/station/package.scala
+++ domain/src/main/scala/com/psisoyev/train/station/package.scala
@@ -1,39 +1,40 @@
 package com.psisoyev.train
 
-import cats.Eq
-import cats.implicits._
-import io.circe.{ Decoder, Encoder }
-import io.estatico.newtype.Coercible
+import derevo.cats.eqv
+import derevo.circe.{ decoder, encoder }
 import io.estatico.newtype.macros.newtype
-import io.estatico.newtype.ops._
+import derevo.derive
 
 import java.time.Instant
 import java.util.UUID
 
 package object station {
+  @derive(decoder, encoder)
   @newtype case class Actual(value: Instant) {
     def toTimestamp: Timestamp = Timestamp(value)
   }
+
+  @derive(decoder, encoder)
   @newtype case class Expected(value: Instant)
+
+  @derive(decoder, encoder)
   @newtype case class Timestamp(value: Instant)
 
+  @derive(decoder, encoder)
   @newtype case class EventId(value: String)
   object EventId {
     def apply(uuid: UUID): EventId = EventId(uuid.toString)
   }
 
+  @derive(decoder, encoder)
   @newtype case class TrainId(value: String)
+
+  @derive(decoder, encoder, eqv)
   @newtype case class City(value: String)
 
+  @derive(decoder, encoder)
   @newtype case class From(city: City)
+
+  @derive(decoder, encoder)
   @newtype case class To(city: City)
-
-  implicit def coercibleEq[A: Coercible[B, *], B: Eq]: Eq[A] =
-    Eq[B].contramap[A](_.asInstanceOf[B])
-
-  implicit def coercibleDecoder[A: Coercible[B, *], B: Decoder]: Decoder[A] =
-    Decoder[B].map(_.coerce[A])
-
-  implicit def coercibleEncoder[A: Coercible[B, *], B: Encoder]: Encoder[A] =
-    Encoder[B].contramap(_.asInstanceOf[B])
 }
