c925e00ee10a868f46d7757d5c7a15a24c8f5032
Pavels Sisojevs <p.sisoyev@gmail.com>
2021-04-13 01:14:51 +0000

    Update zio, zio-test, zio-test-sbt to 1.0.6

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -60,7 +60,7 @@ object Version {
   val cats = "2.3.1"
   val catsEffect = "2.3.1"
   val zioCats = "2.4.0.0"
-  val zio = "1.0.5"
+  val zio = "1.0.6"
   val fs2Core = "2.4.2"
   val kindProjector = "0.11.3"
   val ciris = "1.2.1"
