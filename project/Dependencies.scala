6c31472f1042be9e8a7b7b4e43ae58c47841fbbd
Pavels Sisojevs <p.sisoyev@gmail.com>
2021-06-30 01:09:25 +0000

    Update derevo-cats, derevo-circe to 0.12.6

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -70,7 +70,7 @@ object Version {
   val neutron = "0.0.4"
   val contextApplied = "0.1.4"
   val tofu = "0.10.2"
-  val derevo = "0.12.5"
+  val derevo = "0.12.6"
   val betterMonadicFor = "0.3.1"
   val logback = "1.2.3"
   val log4cats = "1.3.1"
