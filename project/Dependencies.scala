43f0a414b3a18be7651873f410220a8391d7a937
Pavels Sisojevs <p.sisoyev@gmail.com>
2021-12-02 01:11:38 +0000

    Update log4cats-core to 1.4.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -73,5 +73,5 @@ object Version {
   val derevo           = "0.12.8"
   val betterMonadicFor = "0.3.1"
   val logback          = "1.2.6"
-  val log4cats         = "1.3.1"
+  val log4cats         = "1.4.0"
 }
