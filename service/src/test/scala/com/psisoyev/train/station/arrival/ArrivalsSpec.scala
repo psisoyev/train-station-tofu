34be49d9aa6a60d96f779008a5d1fa4b9fe52ce6
Pavels Sisojevs <p.sisoyev@gmail.com>
2023-01-17 01:44:14 +0000

    Applied Scalafix rule(s) github:typelevel/cats-effect/v3_0_0?sha=v3.0.0

--- service/src/test/scala/com/psisoyev/train/station/arrival/ArrivalsSpec.scala
+++ service/src/test/scala/com/psisoyev/train/station/arrival/ArrivalsSpec.scala
@@ -1,6 +1,5 @@
 package com.psisoyev.train.station.arrival
 
-import cats.effect.concurrent.Ref
 import com.psisoyev.train.station.Event.Arrived
 import com.psisoyev.train.station.Generators._
 import com.psisoyev.train.station.arrival.ArrivalValidator.ValidatedArrival
@@ -10,6 +9,7 @@ import zio.interop.catz._
 import zio.test.Assertion._
 import zio.test._
 import zio.test.environment.TestEnvironment
+import cats.effect.Ref
 
 object ArrivalsSpec extends BaseSpec {
   override def spec: ZSpec[TestEnvironment, Failure] =
