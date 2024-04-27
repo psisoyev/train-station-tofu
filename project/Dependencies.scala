cd25e9f0863c9e2f7ffacf0164d21457a780e7ae
Pavels Sisojevs <p.sisoyev@gmail.com>
2024-04-27 00:22:25 +0000

    Update circe-core, circe-generic, ... to 0.14.7

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -65,7 +65,7 @@ object Version {
   val kindProjector    = "0.13.2"
   val ciris            = "1.2.1"
   val http4s           = "1.0-234-d1a2b53"
-  val circe            = "0.14.2"
+  val circe            = "0.14.7"
   val newtype          = "0.4.4"
   val neutron          = "0.0.4"
   val contextApplied   = "0.1.4"
